c1688a191cb566041d8e9218fe9e834ed9e8625e
cordis-dev <darius@adroiti.com>
2019-11-11 15:38:22 +0200

    Create t.ps1

---
+++ src/t.ps1
@@ -0,0 +1,9 @@
+function Get-CustomErrorMessage($ErrorMessage){ $Error = "Error occurred: $ErrorMessage" }
+
+function Get-MeaningOfLife
+{
+	...
+	Write-Host "Computing the answer to the ultimate question of life, the universe and everything"
+	...
+	Write-Host 42
+}
