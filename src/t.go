309823c35c326dee4712cd525cd0ca4420b1b7ec
cordis-dev <darius@adroiti.com>
2022-09-09 11:18:01 +0300

    Update t.go

--- src/t.go
+++ src/t.go
@@ -9,8 +9,4 @@ type Counter uint64
 func AtomicTests() {
 	x := uint64(1)
 	x = atomic.AddUint64(&x, 1)        // ERROR "direct assignment to atomic value"
-	_, x = 10, atomic.AddUint64(&x, 1) // ERROR "direct assignment to atomic value"
-	x, _ = atomic.AddUint64(&x, 1), 10 // ERROR "direct assignment to atomic value"
-
-	y := &x
 }
