7554f8434117eed61cad7852cea431c1e9fd3fb9
cordis-dev <darius@adroiti.com>
2023-08-18 15:43:24 +0300

    Update t.cpp

--- src/t.cpp
+++ src/t.cpp
@@ -1,9 +1,9 @@
 // should not report whitespace/end_of_line
 // should report whitespace/empty_if_body
 size_t TF_DataTypeSize(TF_DataType dt) { 
-  return static_cast<size_t>(
-	if (true)
-	{
+  return static_cast<size_t>( 
+	if (true) 
+	{ 
 	}
 	
     tensorflow::DataTypeSize(static_cast<DataType>(dt)));
