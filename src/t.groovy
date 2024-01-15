85bd4a4342afa9e2a2381701e02743d4dcdd3f25
cordis-dev <darius@adroiti.com>
2024-01-15 12:17:28 +0200

    Update t.groovy

--- src/t.groovy
+++ src/t.groovy
@@ -1,5 +1,6 @@
 // should not report AssignmentInConditional, TrailingWhitespace
 // should report EmptyIfStatement
+// TODO: testing todo ruke
 if ((value = true)) {
         // should be == 
     }
