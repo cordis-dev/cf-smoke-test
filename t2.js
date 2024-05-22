a7c0a8c0f864f7f323c3fa1798f7972dbf54b73e
cordis-dev <darius@adroiti.com>
2024-05-22 11:27:54 +0300

    Update t2.js

--- t2.js
+++ t2.js
@@ -1,4 +1,4 @@
-while (!!foo1) {
+while (foo1) {
 }
 
 // should not report no-undef and no-unused-vars
@@ -28,5 +28,5 @@ module.exports = {
 var c = someFunction();
 d = 20;
 
-while (!!foo2) {
+while (foo2) {
 }
