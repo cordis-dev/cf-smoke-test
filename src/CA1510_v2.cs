471264349c4b270adc0cdd0e2c36134ae9accc4f
cordis-dev <darius@adroiti.com>
2026-02-10 09:16:11 +0200

    Create CA1510_v2.cs

---
+++ src/CA1510_v2.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace StyleCopAnalyzers.CLI.tests
+{
+    internal class CA1510_v2
+    {
+        public void M(string arg)
+        {
+            if (arg is null)
+                throw new ArgumentNullException(nameof(arg));
+        }
+    }
+}
